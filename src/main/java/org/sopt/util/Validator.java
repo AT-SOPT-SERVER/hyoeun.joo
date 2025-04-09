56287fc9fbb859432a754b36b7d40bac63db54ee
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-04-09 23:21:39 +0900

    [#2/feat] 제목이 30자 이상인 경우 예외처리

--- src/main/java/org/sopt/util/Validator.java
+++ src/main/java/org/sopt/util/Validator.java
@@ -2,9 +2,15 @@ package org.sopt.util;
 
 public class Validator {
 
+    private static final int MAX_TITLE_LENGTH = 30;
+
     public static void validateEmptyTitle(String title) {
         if (title == null || title.trim().isEmpty()) {
             throw new IllegalArgumentException("❌ 입력값이 유효하지 않습니다 : 제목이 비어있습니다.");
         }
+        if (title.length() > MAX_TITLE_LENGTH) {
+            throw new IllegalArgumentException("❌ 제목은 " + MAX_TITLE_LENGTH + "자 이하여야 합니다!");
+        }
     }
+
 }
