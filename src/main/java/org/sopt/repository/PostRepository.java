fbb1e78a1020ec422684719fd3a0dad828a62e27
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-04-25 17:22:10 +0900

    [#4/feat] Spring Boot로 전체 구조 변경

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,59 +1,15 @@
 package org.sopt.repository;
 
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    boolean existsByTitle(String title);
 
-public class PostRepository {
-    List<Post> postList = new ArrayList<>();
+    List<Post> findByTitleContaining(String keyword);
 
-    public void save(Post post) {
-        postList.add(post);
-    }
-
-    public List<Post> findAll() {
-        return postList;
-    }
-
-    public Post findPostById(int id) {
-        for (Post post : postList) {
-            if (post.getId() == id) {
-                return post;
-            }
-        }
-        return null;
-    }
-
-    public boolean isExistByTitle(String title) {
-        for (Post post : postList) {
-            //대소문자를 구별하지 않고 확인
-            if (post.getTitle().equalsIgnoreCase(title)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public List<Post> findPostByKeyWord(String keyword) {
-        List<Post> searchResult = new ArrayList<>();
-
-        for (Post post : postList) {
-            if (post.getTitle().contains(keyword)) {
-                searchResult.add(post);
-            }
-        }
-        return searchResult;
-    }
-
-    public boolean delete(int id) {
-        for (Post post : postList) {
-            if (post.getId() == id) {
-                postList.remove(post);
-                return true;
-            }
-        }
-        return false;
-    }
 }
--- src/main/java/org/sopt/util/IdGenerator.java
+++ src/main/java/org/sopt/util/IdGenerator.java
@@ -1,5 +1,6 @@
 package org.sopt.util;
 
+//얘 대신에 @GeneratedValue를 체크해주면 됨
 public class IdGenerator {
     private static int postId = 1;
 
