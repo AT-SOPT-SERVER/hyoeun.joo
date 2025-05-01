30a0a686a80c1dcdf3def840fa156ba1d575d8a4
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-02 04:14:27 +0900

    [#6/add] ErrorMessage 추가

--- src/main/java/org/sopt/global/exception/PostErrorMessage.java
+++ src/main/java/org/sopt/global/exception/PostErrorMessage.java
@@ -5,7 +5,9 @@ public enum PostErrorMessage {
     INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다."),
     POST_ALREADY_EXISTS_ERROR("❌ 이미 존재하는 제목입니다!"),
     POST_NOT_FOUND_ERROR("해당 게시글이 존재하지 않습니다."),
-    POST_NOT_FOUND_KEYWORD_ERROR("🔍 해당 키워드로 검색된 게시글이 없습니다.");
+    POST_NOT_FOUND_KEYWORD_ERROR("🔍 해당 키워드로 검색된 게시글이 없습니다."),
+    POST_UPDATE_FORBIDDEN("수정 권한이 없는 사용자 입니다."),
+    POST_DELETE_FORBIDDEN("삭제 권한이 없는 사용자 입니다.");
 
     private final String message;
 
