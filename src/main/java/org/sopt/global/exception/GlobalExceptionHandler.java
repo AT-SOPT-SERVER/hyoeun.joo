e493fa7171930dddd2093b7f14f18abc9e64f659
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-02 13:26:59 +0900

    [#6/feat] CustomException 구현

--- src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -33,8 +31,8 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntime(RuntimeException e) {
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<?> handleForbidden(ForbiddenException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .body(ResponseUtil.error(403, e.getMessage()));
     }
